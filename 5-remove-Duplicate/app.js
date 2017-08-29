018c026f004668ea5097c10579c9c8023cab7fab
wightman <davidwightmanmusic@gmail.com>
2017-08-29 10:28:26 -0400

    6 merge two sorted arrays

--- 5-remove-Duplicate/app.js
+++ 5-remove-Duplicate/app.js
@@ -2,7 +2,9 @@ var testArray = [1, 2, 3, 4, 4, 4, 5, 5, 6 ,7]
 
 function removeDuplicate(arr) {
 	var uniqueArray = arr.filter(function(item, pos){
-		return arr.indexOf(item) == pos
+		console.log(arr.indexOf(item) + " and " + pos)
+		return arr.indexOf(item) === pos
+
  	})
 	return uniqueArray
 }
---
+++ 6-merge-two-sorted-arrays/app.js
@@ -0,0 +1,23 @@
+var arrOne = [1, 2, 3, 5, 8]
+var arrTwo = [4, 6, 7, 9, 10]
+
+function sortedArray(arr1, arr2){
+	var newSortedArray = []
+		arr1Elment = arr1[0]
+		arr2Element = arr2[0]
+		i=1
+		j=1
+
+	while(arr1Elment || arr2Element){
+		if (arr1Elment < arr2Element || (arr1Elment && !arr2Element)) {
+			newSortedArray.push(arr1Elment);
+			arr1Elment = arr1[i++]
+		} else {
+			newSortedArray.push(arr2Element);
+			arr2Element = arr2[j++];
+		}
+	}
+	return newSortedArray;
+}
+
+console.log(sortedArray(arrOne, arrTwo))
