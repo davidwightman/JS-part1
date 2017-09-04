ac66954ae6e465b9de2e3f515441f2f174d0b675
wightman <davidwightmanmusic@gmail.com>
2017-09-03 22:32:55 -0400

    9 how would you reverse words in a sentence

---
+++ 9-reverse-words/app.js
@@ -0,0 +1,6 @@
+var cat = 'This is the sentence to be reversed'
+
+function reverseWordsInASentence(sentence) {
+	return sentence.split(" ").reverse().join(" ")
+}
+console.log(reverseWordsInASentence(cat))
