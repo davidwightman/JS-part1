0575f5d2375d491b6d04aa9b6e548a67a0ba8b72
wightman <davidwightmanmusic@gmail.com>
2017-09-04 07:58:13 -0400

    10 reverse in place

---
+++ 10-reverse-in-place/app.js
@@ -0,0 +1,6 @@
+var cat = 'This is the sentence to be reversed'
+
+function reverseWordsInPlace(sentence) {
+	return sentence.split("").reverse().join("").split(" ").reverse().join(" ")
+}
+console.log(reverseWordsInPlace(cat))
