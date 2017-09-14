3ec75a0ba2b32dc19cda142cfe38309e2cbc719b
wightman <davidwightmanmusic@gmail.com>
2017-09-14 08:35:43 -0400

    finished 16 sum of two

--- 16-sum-of-two/app.js
+++ 16-sum-of-two/app.js
@@ -1,3 +1,14 @@
-function sumOfTwo (arr, sum) {
+var arrayTest = [1, 2, 4, 6, 7, 8, 9, 10, 11, 12, 13]
+var sumTest = 17
 
+function sumOfTwo (arr, sum) {
+	for (var i=0; i<arr.length-1; i++){
+		for (var j=i+1; j<arr.length-1; j++){
+			if (arr[i] + arr[j] == sum)
+				return true
+		}
+	}
+	return false
 }
+
+console.log(sumOfTwo(arrayTest, sumTest))
