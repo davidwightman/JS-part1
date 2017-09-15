4e41afaf07858e2afe3da494b877e02a18af172a
wightman <davidwightmanmusic@gmail.com>
2017-09-15 08:46:59 -0400

    15 missing number using reduce method

--- 15-missing-number/app.js
+++ 15-missing-number/app.js
@@ -1,5 +1,14 @@
+var arrTest = [1, 3, 5, 2, 6]
+
 function missingNumber (arr) {
 
+var n = arr.length+1;
+var actualSum = arr.reduce(function(sum, value){
+	return sum + value
+}, 0);
+var expectedSum = n* (n+1)/2;
+
+return expectedSum - actualSum;
 }
 
-console.log(missingNumber)
+console.log(missingNumber(arrTest))
