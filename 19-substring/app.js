1ccf627a68aeb51397a8431b8315c9b553f3fbaa
wightman <davidwightmanmusic@gmail.com>
2017-09-18 09:50:17 -0400

    19 started subString

---
+++ 19-substring/app.js
@@ -0,0 +1,8 @@
+var exampleString = 'abbcccddddeeffff';
+var exampleSubString = 'cccddddeef';
+
+function matchSubString (str, subStr) {
+
+}
+
+console.log(matchSubString(exampleString, exampleSubString))
