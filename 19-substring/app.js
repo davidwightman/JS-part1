1eb2c38875317b58f070c51778e8b1c66ea2a22c
wightman <davidwightmanmusic@gmail.com>
2017-09-20 09:46:32 -0400

    19 finished subString

--- 19-substring/app.js
+++ 19-substring/app.js
@@ -2,7 +2,21 @@ var exampleString = 'abbcccddddeeffff';
 var exampleSubString = 'cccddddeef';
 
 function matchSubString (str, subStr) {
+	var idx = 0;
+	j = 0;
 
+	for (i = 0; i<str.length; i++){
+		if(str[i] == subStr[j])
+			j++
+		else
+			j = 0;
+
+		if(j == 0)
+			idx = i;
+		else if (j == subStr.length)
+			return idx
+	}
+	return -1
 }
 
 console.log(matchSubString(exampleString, exampleSubString))
