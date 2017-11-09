0e323f8673bc3389dfd3e7f2f10e8c745392f1c9
wightman <davidwightmanmusic@gmail.com>
2017-11-08 23:18:14 -0500

    fibonacci no. 3

--- 3-how-do-get-nth-fibonacci-number/app.js
+++ 3-how-do-get-nth-fibonacci-number/app.js
@@ -1,8 +1,10 @@
-var nthFibonacciNumber = 5;
+var num = 13;
 
-function nthFibonacciNumber(num){
+function nthFibonacciNumber(num) {
 	var startingNumber = 1;
 
+	if (num <= 1) return num;
+	else return nthFibonacciNumber(num - 1) + nthFibonacciNumber(num - 2);
 }
 
 console.log(nthFibonacciNumber(num));
