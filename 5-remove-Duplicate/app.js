0b6a262aeff6165df07ef2b7929242f7cf8343ff
wightman <davidwightmanmusic@gmail.com>
2017-11-28 23:52:59 -0500

    es6 number 5

--- 5-remove-Duplicate/app.js
+++ 5-remove-Duplicate/app.js
@@ -1,12 +1,11 @@
-var testArray = [1, 2, 3, 4, 4, 4, 5, 5, 6 ,7]
+const testArray = [1, 2, 3, 4, 4, 4, 5, 5, 6, 7];
 
 function removeDuplicate(arr) {
-	var uniqueArray = arr.filter(function(item, pos){
-		console.log(arr.indexOf(item) + " and " + pos)
-		return arr.indexOf(item) === pos
-
- 	})
-	return uniqueArray
+	const uniqueArray = arr.filter(function(item, pos) {
+		// console.log(arr.indexOf(item) + ' and ' + pos);
+		return arr.indexOf(item) === pos;
+	});
+	return uniqueArray;
 }
 
 console.log(removeDuplicate(testArray));
