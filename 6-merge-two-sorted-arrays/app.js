57ad858f9e9b62f97961ee8403df33ab866c8a5e
wightman <davidwightmanmusic@gmail.com>
2017-11-30 18:19:20 -0500

    updated 6 with es6

--- 6-merge-two-sorted-arrays/app.js
+++ 6-merge-two-sorted-arrays/app.js
@@ -1,17 +1,17 @@
-var arrOne = [1, 2, 3, 5, 8]
-var arrTwo = [4, 6, 7, 9, 10]
+const arrOne = [1, 2, 3, 5, 8];
+const arrTwo = [4, 6, 7, 9, 10];
 
-function sortedArray(arr1, arr2){
-	var newSortedArray = []
-		arr1Elment = arr1[0]
-		arr2Element = arr2[0]
-		i=1
-		j=1
+function sortedArray(arr1, arr2) {
+	let newSortedArray = [];
+	arr1Elment = arr1[0];
+	arr2Element = arr2[0];
+	i = 1;
+	j = 1;
 
-	while(arr1Elment || arr2Element){
+	while (arr1Elment || arr2Element) {
 		if (arr1Elment < arr2Element || (arr1Elment && !arr2Element)) {
 			newSortedArray.push(arr1Elment);
-			arr1Elment = arr1[i++]
+			arr1Elment = arr1[i++];
 		} else {
 			newSortedArray.push(arr2Element);
 			arr2Element = arr2[j++];
@@ -20,4 +20,4 @@ function sortedArray(arr1, arr2){
 	return newSortedArray;
 }
 
-console.log(sortedArray(arrOne, arrTwo))
+console.log(sortedArray(arrOne, arrTwo));
