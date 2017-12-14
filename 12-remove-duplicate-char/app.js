0d2066a20d7fdd514dbc81f5c05c3e1daac24eec
wightman <davidwightmanmusic@gmail.com>
2017-12-13 22:08:11 -0500

    es6 updated exercise 12

--- 12-remove-duplicate-char/app.js
+++ 12-remove-duplicate-char/app.js
@@ -1,22 +1,19 @@
-var stringExample = 'this is an example of the string'
+const stringExample = 'this is an example of the string';
 
-function removeDuplicateCharacters(str){
-	var currentCharacter;
-	var checkedCharacters = {};
-	var newString = [];
-	for (var i = 0; i < str.length; i++){
+function removeDuplicateCharacters(str) {
+	let currentCharacter;
+	let checkedCharacters = {};
+	let newString = [];
+	for (let i = 0; i < str.length; i++) {
 		currentCharacter = str[i];
 		if (checkedCharacters[currentCharacter]) {
 			checkedCharacters[currentCharacter]++;
-		} else 
-			checkedCharacters[currentCharacter] = 1;
-		
+		} else checkedCharacters[currentCharacter] = 1;
 	}
-	for (var j in checkedCharacters){
-		if (checkedCharacters[j]==1)
-			newString.push(j);
+	for (let j in checkedCharacters) {
+		if (checkedCharacters[j] == 1) newString.push(j);
 	}
 	return newString.join('');
 }
 
-console.log(removeDuplicateCharacters(stringExample))
+console.log(removeDuplicateCharacters(stringExample));
