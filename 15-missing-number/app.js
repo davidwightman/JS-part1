052d3f30b75508b1a9982fef600f6aa6e41fc453
wightman <davidwightmanmusic@gmail.com>
2017-12-20 16:44:31 -0500

    es6 exercise 15

--- 15-missing-number/app.js
+++ 15-missing-number/app.js
@@ -1,14 +1,13 @@
-var arrTest = [1, 3, 5, 2, 6]
+const arrTest = [1, 3, 5, 2, 6];
 
-function missingNumber (arr) {
+function missingNumber(arr) {
+	var n = arr.length + 1;
+	var actualSum = arr.reduce(function(sum, value) {
+		return sum + value;
+	}, 0);
+	var expectedSum = n * (n + 1) / 2;
 
-var n = arr.length+1;
-var actualSum = arr.reduce(function(sum, value){
-	return sum + value
-}, 0);
-var expectedSum = n* (n+1)/2;
-
-return expectedSum - actualSum;
+	return expectedSum - actualSum;
 }
 
-console.log(missingNumber(arrTest))
+console.log(missingNumber(arrTest));
