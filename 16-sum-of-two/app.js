cfa9f1fdefbe0d8347cae6278c2e8e124e77db1d
wightman <davidwightmanmusic@gmail.com>
2017-12-22 15:18:31 -0500

    es6 exercise 16

--- 16-sum-of-two/app.js
+++ 16-sum-of-two/app.js
@@ -1,14 +1,13 @@
-var arrayTest = [1, 2, 4, 6, 7, 8, 9, 10, 11, 12, 13]
-var sumTest = 17
+const arrayTest = [1, 2, 4, 6, 7, 8, 9, 10, 11, 12, 13];
+const sumTest = 17;
 
-function sumOfTwo (arr, sum) {
-	for (var i=0; i<arr.length-1; i++){
-		for (var j=i+1; j<arr.length-1; j++){
-			if (arr[i] + arr[j] == sum)
-				return true
+function sumOfTwo(arr, sum) {
+	for (let i = 0; i < arr.length - 1; i++) {
+		for (let j = i + 1; j < arr.length - 1; j++) {
+			if (arr[i] + arr[j] == sum) return true;
 		}
 	}
-	return false
+	return false;
 }
 
-console.log(sumOfTwo(arrayTest, sumTest))
+console.log(sumOfTwo(arrayTest, sumTest));
