27c992ba0ae90f9af44898302b4a5ee6d399cbbf
wightman <davidwightmanmusic@gmail.com>
2018-01-06 18:01:06 -0500

    es6 19

--- 19-substring/app.js
+++ 19-substring/app.js
@@ -1,22 +1,18 @@
-var exampleString = 'abbcccddddeeffff';
-var exampleSubString = 'cccddddeef';
+const exampleString = 'abbcccddddeeffff';
+const exampleSubString = 'cccddddeef';
 
-function matchSubString (str, subStr) {
-	var idx = 0;
+function matchSubString(str, subStr) {
+	let idx = 0;
 	j = 0;
 
-	for (i = 0; i<str.length; i++){
-		if(str[i] == subStr[j])
-			j++
-		else
-			j = 0;
+	for (i = 0; i < str.length; i++) {
+		if (str[i] == subStr[j]) j++;
+		else j = 0;
 
-		if(j == 0)
-			idx = i;
-		else if (j == subStr.length)
-			return idx
+		if (j == 0) idx = i;
+		else if (j == subStr.length) return idx;
 	}
-	return -1
+	return -1;
 }
 
-console.log(matchSubString(exampleString, exampleSubString))
+console.log(matchSubString(exampleString, exampleSubString));
